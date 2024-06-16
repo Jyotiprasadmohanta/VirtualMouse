add0b6896945ce2d28e0f506e65c70d0d1c50c92
jyotiprasadmohanta <jyotiprasadmohanta@gmail.com>
2024-06-16 23:07:50 +0530

    update: flip the camera frame and check the x,y cordinatins

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ drawing_utils = mp.solutions.drawing_utils
 hand_detector = mp.solutions.hands.Hands()
 while True:
     _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
     if hands:
         for hand in hands:
             drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmarks in enumerate(landmarks):
+                x = landmarks.x
+                y = landmarks.y
+                print(x,y)
+
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
