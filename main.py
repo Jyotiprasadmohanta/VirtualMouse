859f7e88b222d6b07ccf45bb0fa9d99a292220e0
jyotiprasadmohanta <jyotiprasadmohanta@gmail.com>
2024-06-17 00:20:09 +0530

    Update: Add click function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ cap = cv2.VideoCapture(0)
 drawing_utils = mp.solutions.drawing_utils
 hand_detector = mp.solutions.hands.Hands()
 screen_width, screen_height = pyautogui.size()
+index_y = 0
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -20,11 +21,18 @@ while True:
             for id, landmarks in enumerate(landmarks):
                 x = int(landmarks.x*frame_width)
                 y = int(landmarks.y*frame_height)
-                print(x,y)
+                # print(x,y)
                 if id == 8:
-                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0,255,255))
+                    cv2.circle(img=frame, center=(x,y), radius=20, color=(0,255,255))
                     index_x = screen_width/frame_width*x
                     index_y = screen_height/frame_height*y
                     pg.moveTo(index_x, index_y)
+                if id == 4:
+                    cv2.circle(img=frame, center=(x, y), radius=20, color=(0, 0, 255))
+                    thumb_x = screen_width / frame_width*x
+                    thumb_y = screen_height / frame_height*y
+                    print('outside', abs(index_y - thumb_y))
+                    if abs(index_y - thumb_y) < 20:
+                        print('click')
     cv2.imshow('Virtual Mouse', frame)
     cv2.waitKey(1)
